30f68ac2b7a4251fd16355db1914d913a5582a64
Zubin <me@zubin.org.in>
2023-03-16 00:52:44 +0530

    Fix Streamtape 4 seconds requirement

--- src/index.js
+++ src/index.js
@@ -8,37 +8,36 @@
 import fetch from "node-fetch";
 
 async function download(url, user, pass) {
-  // File
-
   let match = url.match(/(https?:\/\/[^\s]+)/g);
   let link = match[0];
   let file = link.split("/v/")[1];
   console.log("Contacting API. Starting download.");
-  let downloadLink;
 
-  // Ticket
-
-  await fetch(
+  let ticketRes = await fetch(
     `https://api.streamtape.com/file/dlticket?file=${file}&login=${user}&key=${pass}`
-  )
-    .then((res) => res.json())
-    .then((body) => {
-      const data = body.result;
-      let ticket = data.ticket;
+  );
+  let ticketData = await ticketRes.json();
+  let ticket = ticketData.result.ticket;
 
-      // Download
+  let downloadLink;
+  setTimeout(async () => {
+    let downloadRes = await fetch(
+      `https://api.streamtape.com/file/dl?file=${file}&ticket=${ticket}`
+    );
+    let downloadData = await downloadRes.json();
+    downloadLink = downloadData.result.url;
+  }, 5000);
 
-      setTimeout(async () => {
-        await fetch(
-          `https://api.streamtape.com/file/dl?file=${file}&ticket=${ticket}`
-        )
-          .then((res) => res.json())
-          .then((body) => {
-            downloadLink = body.result.url;
-          });
-      }, 5000);
-    });
-  return downloadLink;
+  return new Promise((resolve) => {
+    const checkDownloadLink = () => {
+      if (downloadLink) {
+        resolve(downloadLink);
+      } else {
+        setTimeout(checkDownloadLink, 1000);
+      }
+    };
+    checkDownloadLink();
+  });
 }
 
 export default { download };
