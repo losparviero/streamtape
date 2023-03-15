6e6ccab73103caa99e9ca728c58dc8fd5faf3fc6
Zubin <me@zubin.org.in>
2023-03-16 01:34:57 +0530

    Integrate test

---
+++ test/README.md
@@ -0,0 +1,15 @@
+# Tests
+
+```
+npm test
+```
+
+1. Rename example.env to .env and provide API credentials.
+
+2. Install devDependencies
+
+```
+npm install --only=dev
+```
+
+2. Upon running test, provide Streamtape link.
--- test/test.js
+++ test/test.js
@@ -9,7 +9,8 @@ const url = "https://streamtape.com/v/kP30d22qLZi06D";
 async function test() {
   await st
     .download(url, process.env.API_USER, process.env.API_PASS)
-    .then(console.log);
+    .then(console.log)
+    .catch((error) => console.log(error));
 }
 
 test();
